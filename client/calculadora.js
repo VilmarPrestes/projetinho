5021cf0488d5ca563d5cca8fed398d906b82b98b
Gabriel <rabeloglr@gmail.com>
2023-08-13 17:15:11 -0300

    calculadora botoes fim

--- client/calculadora.js
+++ client/calculadora.js
@@ -3,8 +3,17 @@ let resultado = document.querySelector('#resultado');
 
 function calcular(termo) {
   if (termo == '=') equacao = eval(equacao);
-  else {
-    equacao += termo;
-    resultado.innerHTML = equacao;
-  }
+  else equacao += termo;
+
+  resultado.value = equacao;
+}
+
+function limpar() {
+  equacao = '';
+  resultado.value = equacao;
+}
+
+function apagar() {
+  equacao = equacao.slice(0, -1);
+  resultado.value = equacao;
 }
